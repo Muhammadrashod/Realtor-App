2bb67e0139285ef0b6e022bdd175eeda75a72d76
Muhammadrashod Akromov <rashod0730@icloud.com>
2024-01-09 12:46:58 +0500

    workingLogin

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Header } from "../../components/UI/Header/Header";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import IconButton from "@mui/material/IconButton";
@@ -18,56 +19,59 @@ import { Heading } from "../../components/Typography/Heading";
 import { Container } from "../../components/UI/Container/Container.style";
 
 export const ProfilePage = () => {
-  const storedDataString = localStorage.getItem("registrationFormData" || "loginFormData");
+  const location = useLocation();
+  const userData = location.state?.userData || {};
 
-  const storedData = storedDataString ? JSON.parse(storedDataString) : null;
-
-  const username = storedData ? storedData[0] : "N/A";
-  const useremail = storedData ? storedData[2] : "N/A";
+  const username = userData?.useremail || "N/A";
+  const userpassword = userData?.userpassword || "N/A";
+  const userlastname = userData?.userlastname || "N/A";
+  const userage = userData?.userage || "N/A";
+  const usercity = userData?.usercity || "N/A";
+  // Add more properties as needed
 
   return (
     <>
       <Header />
-    <Container>
-      <AvatarTextContainer>
-      <Heading headingText={`${username}`} headingType="h3"/>
-      </AvatarTextContainer>
-      <HeadingContainer>
-        <Heading headingText="Ваш Профиль" headingType="h1" />
-      </HeadingContainer>
+      <Container>
+        <AvatarTextContainer>
+          <Heading headingText={`${username}`} headingType="h3" />
+        </AvatarTextContainer>
+        <HeadingContainer>
+          <Heading headingText="Ваш Профиль" headingType="h1" />
+        </HeadingContainer>
 
-       <StyledProfilePage> 
+        <StyledProfilePage>
           <StyledAvatar />
-        <InfoContainer>
-          <InfoText>
-            <Heading headingText={`Имя: ${username}`} />
-            <Heading headingText={`Фамилия: ${useremail}`} />
-            <Heading headingText={`Возраст: ${useremail}`} />
-            <Heading headingText={`Почта: ${useremail}`} />
-            <Heading headingText={`Пароль: ${useremail}`} />
-            <Heading headingText={`Город: ${useremail}`} />
-          </InfoText>
-        </InfoContainer>
-       </StyledProfilePage>
+          <InfoContainer>
+            <InfoText>
+              <Heading headingText={`Почта: ${username}`} />
+              <Heading headingText={`Пароль: ${userpassword}`} />
+              <Heading headingText={`Фамилия: ${userlastname}`} />
+              <Heading headingText={`Возраст: ${userage}`} />
+              <Heading headingText={`Город: ${usercity}`} />
+              {/* Add more lines for other user data */}
+            </InfoText>
+          </InfoContainer>
+        </StyledProfilePage>
 
-       <StyledParametrs>
-        <Heading headingText="Text"/>
-        <IconContainer>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <Heading headingText="Избранные" headingType="h3"/>
-          <IconButton aria-label="settings">
-            <Settings />
-          </IconButton>
-          <Heading headingText="Настройки" headingType="h3"/>
-          <IconButton aria-label="Logout">
-            <LogoutIcon />
-          </IconButton>
-        </IconContainer>
-        <Heading headingText="Выйти" headingType="h3"/>
-       </StyledParametrs>
-    </Container>
+        <StyledParametrs>
+          <Heading headingText="Text" />
+          <IconContainer>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <Heading headingText="Избранные" headingType="h3" />
+            <IconButton aria-label="settings">
+              <Settings />
+            </IconButton>
+            <Heading headingText="Настройки" headingType="h3" />
+            <IconButton aria-label="Logout">
+              <LogoutIcon />
+            </IconButton>
+          </IconContainer>
+          <Heading headingText="Выйти" headingType="h3" />
+        </StyledParametrs>
+      </Container>
     </>
   );
 };
